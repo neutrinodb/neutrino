7342c4a5dfa7e3a254e0c77c1d72ab6ee8af95d8
Andre Carlucci <Andre Carlucci>
2016-07-15 18:56:53 -0300

    Correcting gitignore mistake

---
+++ src/Neutrino.Api/Controllers/OccurrencesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Neutrino;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Neutrino.Api.Controllers {
+    [Route("api/[controller]")]
+
+    public class OccurrencesController {
+        private ITimeSerieService _service;
+
+        public OccurrencesController(ITimeSerieService service) {
+            _service = service;
+        }
+
+        [HttpGet]
+        public string Get() {
+            return "Hello, fella. I'm the Occurrences endpoint.";
+        }
+
+        [Route("{*id}")]
+        public async Task Post(string id, [FromBody] List<Occurrence> occurrences) {
+            await _service.Save(id, occurrences);
+        }
+
+        [HttpGet]
+        [Route("{*id}")]
+        public async Task<TimeSerie> Get(string id, DateTime start, DateTime end) {
+            return await _service.List(id, start, end);
+        }
+    }
+}
